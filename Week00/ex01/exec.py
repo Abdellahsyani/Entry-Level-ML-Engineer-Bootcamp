d040b55b7cb52dd7b37f2b971a95c078f826d563
Abdellahsyani <syaniabdos@gmail.com>
2025-03-03 16:59:16 +0000

    add ex01

--- Week00/ex01/exec.py
+++ Week00/ex01/exec.py
@@ -7,3 +7,11 @@ def reverse_f(text):
     for i in range(1, len(text) + 1):
         tex += text[len(text) - i]
     return tex
+
+reverse_words = []
+for word in args:
+    reverse_word = reverse_f(word)
+    reverse_words.append(reverse_word)
+res = ' '.join(reverse_words)
+
+print(res)
