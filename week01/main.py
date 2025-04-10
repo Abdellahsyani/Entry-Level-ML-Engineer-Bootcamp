59ba9b761d6d981d7fe56097e8dabe0f9b49efbe
Abdellahsyani <syaniabdos@gmail.com>
2025-04-10 20:28:19 +0100

    scraped data

--- week01/main.py
+++ week01/main.py
@@ -1,18 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://realpython.github.io/fake-jobs/jobs/senior-python-developer-0.html"
+URL = "https://www.octoparse.com/template/amazon-product-scraper-by-keywords"
 
 page = requests.get(URL)
+
 soup = BeautifulSoup(page.content, "html.parser")
-res = soup.find(id="ResultsContainer")
 
-job_cards = res.find_all("div", class_="box")
+# cards = soup.find_all("div", class_="table-scroll-box")
+cards = soup.find_all("div", class_="t-max-height")
 
-for card in job_cards:
-    title = card.find("h1", class_="title is-2")
-    company = card.find("h2", class_="company")
-    loc = card.find("p", id="location")
-    print(title.text)
-    print(company.text)
-    print(loc.text)
+for card in cards:
+    print(card.text)
