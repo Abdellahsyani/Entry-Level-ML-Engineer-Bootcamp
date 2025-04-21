129c629d1e7deba322693c78be9fb013c0569f48
Abdellahsyani <syaniabdos@gmail.com>
2025-04-21 17:41:42 +0100

    finished

--- Week00/ex07/scrapper.py
+++ Week00/ex07/scrapper.py
@@ -6,18 +6,13 @@ URL = "https://data.1337ai.org/"
 
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
-tr = soup.find_all('tr')
+rows = soup.find_all('tr')
+data = []
+headers = [th.text for th in rows[0].find_all('th')]
 
-list1 = []
-
-for tag in tr:
-    li = []
-    th = tag.find_all('th')
-    td = tag.find_all('td')
-    li.append([tag.text for tag in th])
-    li.append([tag.text for tag in td])
-    if (li):
-        list1.append(li)
-
-panda = pd.DataFrame(list1)
+for tag in rows[1:]:
+    values = [td.text.strip() for td in tag.find_all('td')]
+    if len(headers) == len(values):
+        data.append(dict(zip(headers, values)))
+panda = pd.DataFrame(data)
 panda.to_csv('data.csv', index=False)
